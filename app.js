703fe61cc90c266f7a573ecc26eac28cbe948a33
Tomoya <flyingfox193807@gmail.com>
2024-11-25 12:03:15 -0800

    finished exercise

--- app.js
+++ app.js
@@ -1,3 +1,41 @@
-$(function() {
-  // YOUR CODE HERE
-})
+  $(function() {
+    $("#toggleButton").click(function(){
+      $("#myParagraph").toggle();
+    });
+  });
+
+  $(function() {
+    $("#colorButton").click(function() {
+      $("#colorDiv").css(
+        "background-color", "yellow"
+      )
+    })
+  })
+
+  $(function() {
+    $("#addClassButton").click(function() {
+      $("#classDiv").addClass("newClass")
+    })
+    $("#removeClassButton").click(function() {
+      $("#classDiv").removeClass("newClass")
+    })
+  })
+
+  $(function() {
+    $("#fadeInButton").click(function() {
+      $("#fadeDiv").fadeIn()
+    })
+    $("#fadeOutButton").click(function() {
+      $("#fadeDiv").fadeOut()
+    })
+  })
+
+  $(function() {
+    $("#slideUpButton").click(function() {
+      $("#slideDiv").slideUp()
+    })
+    $("#slideDownButton").click(function() {
+      $("#slideDiv").slideDown()
+    })
+  })
+
